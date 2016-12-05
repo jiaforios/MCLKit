bdd1ad40272ecb6430b09d8ec803020f693110dd
jiaforios <jiaforios@163.com>
2016-12-05 09:57:17 +0800

    优化方法执行策略
    优化方法执行策略

--- MCLKit/MZKit+Config.m
+++ MCLKit/MZKit+Config.m
@@ -13,10 +13,15 @@
 - (MZKit *(^)(CGRect))m_frame
 {
     return ^(CGRect frame){
-        if ([self.someObj isKindOfClass:[UIView class]]) {
+        NSString *funcstr = [NSString stringWithUTF8String:sel_getName(_cmd)];
+        funcstr = [funcstr substringFromIndex:[funcstr rangeOfString:@"m_"].length];
+        SEL sel = [self creatSetterWithPropertyName:funcstr];
+        if ([self.someObj respondsToSelector:sel]) {
+//            [self.someObj setValue:[NSValue valueWithCGRect:frame] forKey:funcstr];
+            IMP imp = [self.someObj methodForSelector:sel];
+            void (*func)(id, SEL, CGRect ) = (void *)imp;
+            func(self.someObj, sel, frame);
             
-            UIView *view = (UIView *)self.someObj;
-            view.frame = frame;
         }
         return self;
     };
@@ -25,71 +30,53 @@
 - (void)funcStr:(NSString *)str andObj:(id)obj resultBlock:(void(^)(SEL))resBlock
 {
     while ([[obj class] isSubclassOfClass:[NSObject class]]) {
-        NSLog(@"class = %@",NSStringFromClass([obj class]));
         unsigned int count = 0;
         objc_property_t *propertys = class_copyPropertyList([self.someObj class], &count);
         for (int i = 0; i<count; i++) {
             objc_property_t property = propertys[i];
             const char *name = property_getName(property);
             NSString *ocName = [NSString stringWithUTF8String:name];
-            NSLog(@"ocname = %@",ocName);
             if ([str containsString:ocName]) {
                 SEL sel =  [self creatSetterWithPropertyName:ocName];
+                
                 if ([self.someObj respondsToSelector:sel]) {
-                    NSLog(@"响应方法");
                     resBlock?resBlock(sel):nil;
                     free(propertys);
                     return;
                 }else
                 {
-                    NSLog(@"不响应方法");
                     resBlock?resBlock(nil):nil;
-                    
-
                 }
             }
             
         }
-        
         obj = [obj superclass];
     }
-
-
-    
 }
 - (MZKit *(^)(NSString*))m_text
 {
     return ^(NSString *text){
         NSString *funcStr = [NSString stringWithUTF8String:sel_getName(_cmd)];
         [self funcStr:funcStr andObj:self.someObj resultBlock:^(SEL sel) {
-            [self.someObj performSelector:sel withObject:text];
+            IMP imp = [self.someObj methodForSelector:sel];
+            void (*func)(id, SEL, NSString *) = (void *)imp;
+            func(self.someObj, sel, text);
+            
         }];
         return self;
     };
 }
 
-
-
-- (SEL) creatSetterWithPropertyName:(NSString *) propertyName{
-    
-    //1.首字母大写
+- (SEL)creatSetterWithPropertyName:(NSString *) propertyName{
     propertyName = propertyName.capitalizedString;
-    
-    //2.拼接上set关键字
     propertyName = [NSString stringWithFormat:@"set%@:", propertyName];
-    
-    //3.返回set方法
     return NSSelectorFromString(propertyName);
 }
 
 - (MZKit *(^)(UIColor *))m_textColor
 {
     return ^(UIColor *color){
-        
-//        NSString *type = objc_getAssociatedObject(self, &objectkeytype);
-//        UILabel *label  = objc_getAssociatedObject(self, &objectkey);
-//        label.textColor = color;
-        
+
         return self;
     };
 }
@@ -97,11 +84,6 @@
 -(MZKit *(^)(UIColor *))m_backgroundColor
 {
     return ^(UIColor *color){
-        
-        //        NSString *type = objc_getAssociatedObject(self, &objectkeytype);
-//        UILabel *label  = objc_getAssociatedObject(self, &objectkey);
-//        label.backgroundColor = color;
-        
         return self;
     };
 }
